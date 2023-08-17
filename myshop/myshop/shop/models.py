793231ab915a37853b9ee3d4992148114567ecf9
Savinsky <savinsky.n@gmail.com>
2023-08-17 15:58:55 +0400

    Django project

--- myshop/myshop/shop/models.py
+++ myshop/myshop/shop/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Категория')
+    slug = models.SlugField(max_length=200, db_index=True, unique=True, verbose_name='URL')
 
     class Meta:
         ordering = ('name',)
@@ -12,12 +12,15 @@ class Category(models.Model):
         verbose_name_plural = 'Категории'
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug': self.slug})
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    cat = models.ForeignKey('Category',on_delete=models.PROTECT)
+    title = models.SlugField(max_length=200)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', null = True, blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
@@ -30,4 +33,6 @@ class Product(models.Model):
         index_together = (('id', 'slug'),)
 
     def __str__(self):
-        return self.name
+        return self.title
+    def get_absolute_url(self):
+        return reverse('prods', kwargs={'prods_slug': self.slug})
--- myshop/myshop/shop/views.py
+++ myshop/myshop/shop/views.py
@@ -1,3 +1,124 @@
-from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from .models import Category, Product
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from .forms import LoginForm, RegisterUserForm, AddProductForm, RegisterUserForm
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
+from .utils import DataMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+
+class MainPage(DataMixin, ListView):
+    model = Product
+    template_name = 'myshop/index.html'
+    context_object_name = 'prods'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title= "Главная страница интернет-магазина")
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
+    def get_queryset(self):
+        return Product.objects.filter(available = True)
+
+
+class ShopPage(ListView):
+    model = Product
+    template_name = 'myshop/shop.html'
+class CartPage(DetailView):
+    #model =
+    template_name = 'myshop/cart.html'
+class CheckoutPage(FormView):
+    template_name = 'myshop/checkout.html'
+class ShowProduct(DataMixin, DetailView):
+    model = Product
+    template_name = 'myshop/single-product.html'
+    slug_url_kwarg = 'product_slug'
+    context_object_name = 'product'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title=context['products'])
+        return dict(list(context.items()) + list(c_def.items()))
+    def get_object(self, **kwargs):
+        object = super().get_object(**kwargs)
+        return object
+
+class AddProduct(LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddProductForm
+    template_name = 'myshop/product_add.html'
+    success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
+    #raise_exception = True #запрет доступа к странице либо декоратор для функции @login_required
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Добавление товара')
+        return dict(list(context.items()) + list(c_def.items()))
+
+class ProductCategory(DataMixin, ListView):
+    model = Category
+    template_name = 'myshop/index.html'
+    context_object_name = 'prods'
+    allow_empty = False
+    def get_queryset(self):
+        return Product.objects.filter(cat__clug=self.kwargs['cat_slug'], is_published=True)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Категория - ' + str(context['products'][0].cat),
+                                      cat_selected=context['products'][0].cat_id)
+        return dict(list(context.items()) + list(c_def.items()))
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'myshop/single-product.html'
+
+"""
+class CategoryUpdateView(UpdateView):
+    template_name = 'myshop/category_update.html'
+    model = Category
+    success_url = reverse_lazy('myshop:category_list')
+
+class CategoryDeleteView(DeleteView):
+    template_name = 'myshop/category_delete.html'
+    model = Category
+    success_url = reverse_lazy('myshop:category_list')
+
+class CategoryCreateView(CreateView):
+    fields = '__all__'
+    template_name = 'myshop/category_create.html'
+    model = Category
+    success_url = reverse_lazy('myshop:category_list') """
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'myshop/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+class LoginUser(DataMixin, LoginView):
+    form_class = AuthenticationForm
+    template_name = 'myshop/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
